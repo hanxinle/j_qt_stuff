96383621d6498d8297c949eacde19b588236f28f
hanxinle <hanxinle199@gmail.com>
2023-01-31 23:16:00 +0800

    状态栏完成

--- qt_learn_xl/03_statusbar/statusbar.cpp
+++ qt_learn_xl/03_statusbar/statusbar.cpp
@@ -1,10 +1,12 @@
 #include "statusbar.h"
 
 statusbar::statusbar(QWidget *parent)
-    : QMainWindow(parent)
-{
+    : QMainWindow(parent) {
     ui.setupUi(this);
+
+    connect(ui.pushButton, &QPushButton::clicked, this, [=]() {
+        statusBar()->showMessage("btn cliced",2000);
+    });
 }
 
-statusbar::~statusbar()
-{}
+statusbar::~statusbar() {}
