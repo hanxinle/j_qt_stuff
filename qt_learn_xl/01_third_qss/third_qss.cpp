3e18eca167e5ab24ab031d5ac4c4815e7119a02e
hanxinle <hanxinle199@gmail.com>
2023-02-06 17:05:53 +0800

    资源文件打包与隐藏 1. rcc 命令的使用在 .cpp 中有注释； 2. 在原来工程基础上更改，去除 qresoure 相关，恢复注释文件即可还原工程. 3. 本工程没有删除原有的图片等文件，使用 rcc 打包后理论上是可以删除的.

--- qt_learn_xl/01_third_qss/third_qss.cpp
+++ qt_learn_xl/01_third_qss/third_qss.cpp
@@ -2,18 +2,28 @@
 #include <qfile.h>
 #include <qtextstream.h>
 #include <qiodevice.h>
+#include <qresource.h>
+
+/*================================================================
+* 如果不使用 loadQssFile 函数暴露注释掉的资源文件 I:\\J_qt-stuff\\qt_learn_xl\\01_third_qss\\style.qss.
+* 采用的方法是封装资源文件到 rcc 中，loadQssFile 使用的是 qrc 指明的资源文件,前提是注册 res.rcc 资源文件.
+* 使用的资源打包命令是
+* rcc -binary third_qss.qrc -o res.rcc
+=================================================================*/
 
 third_qss::third_qss(QWidget *parent)
     : QMainWindow(parent) {
     ui.setupUi(this);
-
+    QString path = "I:\\J_qt-stuff\\qt_learn_xl\\01_third_qss\\res.rcc";
+    QResource::registerResource(path);
     loadQssFile();
 }
 
 third_qss::~third_qss() {}
 
 void third_qss::loadQssFile() {
-    QFile file("I:\\qt-stuff\\qt_learn_xl\\01_third_qss\\style.qss");
+    //QFile file("I:\\J_qt-stuff\\qt_learn_xl\\01_third_qss\\style.qss");
+    QFile file(":/qss_file/style.qss");
     QString str;
 
     if ((file.open(QIODevice::ReadOnly | QIODevice::Text))) {
