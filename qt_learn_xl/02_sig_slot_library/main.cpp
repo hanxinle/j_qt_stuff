93ad8ffc949e5eac06d58eb472d6470e69e60da0
hanxinle <hanxinle199@gmail.com>
2023-02-09 21:49:21 +0800

    使用一个信号槽的库定义完成案例

--- qt_learn_xl/02_sig_slot_library/main.cpp
+++ qt_learn_xl/02_sig_slot_library/main.cpp
@@ -1,8 +1,36 @@
+#include "sigslot.h"
 #include <QtCore/QCoreApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
+// using namespace sigslot;
+// 从定义文件可以看到，sig 的一系列定义的不同点在于模板参数的个数.
+
+
+struct Window {
+    sigslot::signal0<> sig0;
+    sigslot::signal1<double> sig1;
+    sigslot::signal<> sig;
+};
+
+struct MySlots : public sigslot::has_slots<> {
+    void on_sig() { std::cout << "on_sig" << std::endl; }
+    void on_sig0() { std::cout << "on_sig0" << std::endl; }
+    void on_sig1(double v) { std::cout << "on_sig1 = " << v << std::endl; }
+};
+
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+
+    Window w;
+    MySlots slts;
+
+    w.sig.connect(&slts, &MySlots::on_sig);
+    w.sig0.connect(&slts,&MySlots::on_sig0);
+    w.sig1.connect(&slts,&MySlots::on_sig1);
+
+    w.sig();
+    w.sig0();
+    w.sig1(1123.44);
 
     return a.exec();
 }
